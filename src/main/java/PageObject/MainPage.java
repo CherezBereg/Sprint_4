a043337e97a2e814827b9bb8da7bc30048b4414d
CherezBereg <117637101+CherezBereg@users.noreply.github.com>
2022-12-15 13:25:06 +0300

    IDE

---
+++ src/main/java/PageObject/MainPage.java
@@ -0,0 +1,29 @@
+package PageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+public class MainPage {
+    private WebDriver driver;
+    public MainPage (WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public final String url = "https://qa-scooter.praktikum-services.ru/";
+    // Кнопка, убирающая запрос на куки
+    private final By cookieButtonYes = By.xpath("//*[@id=\"rcc-confirm-button\"]");
+    // Аккордеон с вопросами
+    private final By accordionQuestions = By.xpath("//*[@id=\"root\"]/div/div/div[5]/div[2]/div");
+
+    // Скролим до вопросов
+    public void scrollQuestions() {
+        WebElement accordion = driver.findElement(accordionQuestions);
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", accordion);
+    }
+    // Соглашаемся на куки
+    public void pushCookieButtonYes(){
+        driver.findElement(cookieButtonYes).click();
+    }
+}
